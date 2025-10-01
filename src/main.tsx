825a03aa2f28ae036e8827883d9582cc21db1d31
Cleyton Sousa <cleytonsousadossantos.2021@gmail.com>
2025-10-01 13:25:38 -0300

    add tagging to site

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {App} from './App.tsx'
 import "./index.css"
+import { initRybbit, trackEvent, RybbitEvents } from './monitoring/rybbit'
+
+initRybbit();
+
+trackEvent(RybbitEvents.APP_LOADED, {
+  timestamp: new Date().toISOString(),
+  userAgent: navigator.userAgent,
+  language: navigator.language,
+  platform: navigator.platform
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
