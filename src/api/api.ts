b5a57d815ea435ca070f9cd64adaf8fb0b196932
Cleyton Sousa <cleytonsousadossantos.2021@gmail.com>
2025-12-07 12:41:09 -0300

    fix api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-// Define a URL base da API
-// Se a variável de ambiente estiver definida, usa ela
-// Caso contrário, usa a URL relativa que funcionará tanto em desenvolvimento quanto em produção
-const apiUrl = import.meta.env.VITE_NOFAKE_PNG_API_URL || 
-               (window.location.protocol + '//' + window.location.hostname + 
-               (window.location.hostname === 'localhost' ? ':8000' : '/api'));
-
 export const api = axios.create({
-    baseURL: apiUrl
+    baseURL: import.meta.env.VITE_NOFAKE_PNG_API_URL
 })
